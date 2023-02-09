e65ffda89beb3a38642f8579574b6bcd88b90e56
Yagmur Sahin <yagmurs@hotmail.com>
2023-02-09 19:59:43 +0300

    comments and reference link

--- automation/Set-ManagedIdentityDirectoryRead.ps1
+++ automation/Set-ManagedIdentityDirectoryRead.ps1
@@ -1,9 +1,16 @@
+<#
+    reference article
+
+    https://techcommunity.microsoft.com/t5/integrations-on-azure-blog/grant-graph-api-permission-to-managed-identity-object/ba-p/2792127#:~:text=Managed%20identities%20provide%20an%20identity%20for%20applications%20to,principal%20object%20in%20Azure%20or%20using%20Managed%20Identity.
+
+#>
+
 $tenantID="<Tenant Id>"
 $graphAppId = "00000003-0000-0000-c000-000000000000"
 $displayNameOfManagedIdentityName="<Automation-Account-Name>"
 $permissionName = "Directory.Read.All"
 
-Connect-AzureAD -TenantId $TenantID
+Connect-AzureAD -TenantId $TenantID #this required for last line
 $managedIdentity = (Get-AzADServicePrincipal -Filter "displayName eq '$displayNameOfManagedIdentityName'")
 #Start-Sleep -Seconds 10
 $graphServicePrincipal = Get-AzADServicePrincipal -Filter "appId eq '$graphAppId'"
